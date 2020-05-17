683b61523dc9bb36d26cd0fe3ba5c7bbff7f7892
Abel Riboulot <tkai@protomail.com>
2020-05-17 08:35:46 -0700

    no og:image

--- components/layout.js
+++ components/layout.js
@@ -29,13 +29,9 @@ export default function Layout({ children }) {
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="TODO"
+          content="How-to, data-science, and dogs"
         />
         <meta name="viewport" content="width=device-width"/>
-        <meta
-          property="og:image"
-          content={`TODO`}
-        />
         <meta name="og:title" content={siteTitle} />
       </Head>
 
